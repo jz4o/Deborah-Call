eefe63a61ff53bf8ee08c8ddeddf9ed2b9f721c3
Shoya　Ueda <flum1278pool@gmail.com>
2019-03-21 01:14:39 +0900

    お問い合わせ新規登録画面が大体完了した。

--- Controllers/InquryController.cs
+++ Controllers/InquryController.cs
@@ -25,17 +25,58 @@ namespace InquryController
         [Route("Inqury/New")]
         public IActionResult New()
         {
-            ViewBag.system = Date_Fetch("Mst_System");
+            ViewBag.system = Fetch_System();
+            ViewBag.com = Fetch_Communication();
+            ViewBag.user = Fetch_User();
+            ViewBag.type = Fetch_Type();
+            ViewBag.login = HttpContext.Session.GetString("login");
             return View();
         }
 
-        public Dictionary<int, string> Date_Fetch(string table_name)
+        //システム名をディクショナリ型で生成します。
+        public Dictionary<int, string> Fetch_System()
         {
             Dictionary<int, string> dic = new Dictionary<int, string>();
             var _result = this._context.Mst_System.OrderBy(r => r.Id);
             foreach (var item in _result)
             {
                 dic.Add(item.Id, item.System_name);
+            }
+            return dic;
+        }
+
+        //連絡方法をディクショナリ型で返します。
+        public Dictionary<int, string> Fetch_Communication()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            var _result = this._context.Mst_Communication.OrderBy(r => r.Id);
+            foreach (var item in _result)
+            {
+                dic.Add(item.Id, item.Com_Name);
+            }
+            return dic;
+        }
+
+        //ユーザ情報をディクショナリ型で返します。
+        public Dictionary<int, string> Fetch_User()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            var _result = this._context.Mst_User.OrderBy(r => r.Id);
+            foreach (var item in _result)
+            {
+                dic.Add(item.Id, item.User_Name);
+            }
+            return dic;
+        }
+        
+        //問合せ分類をディクショナリ型で返します。
+        public Dictionary<int, string> Fetch_Type()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            var _result = this._context.Mst_Type.OrderBy(r => r.Id);
+            foreach (var item in _result)
+            {
+                dic.Add(item.Id, item.Type_Name);
             }
             return dic;
         }
