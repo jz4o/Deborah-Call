af492bdbebf4cb5f5a9011a9e7ad7803a83f6ac3
Shoya　Ueda <flum1278pool@gmail.com>
2019-03-21 00:06:24 +0900

    system select add

--- Controllers/InquryController.cs
+++ Controllers/InquryController.cs
@@ -25,8 +25,19 @@ namespace InquryController
         [Route("Inqury/New")]
         public IActionResult New()
         {
-            Console.WriteLine(DateTime.Now);
+            ViewBag.system = Date_Fetch("Mst_System");
             return View();
+        }
+
+        public Dictionary<int, string> Date_Fetch(string table_name)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            var _result = this._context.Mst_System.OrderBy(r => r.Id);
+            foreach (var item in _result)
+            {
+                dic.Add(item.Id, item.System_name);
+            }
+            return dic;
         }
     }
 }
