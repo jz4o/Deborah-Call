e87dabb91a3235d7aaf832c31a72e69b3056ad50
kazusapon <flum1278pool@gmail.com>
2019-03-22 16:11:52 +0900

    モデル修正

--- Models/Deborah.cs
+++ Models/Deborah.cs
@@ -1,34 +1,45 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Deborah.Models
 {
     public class Mst_System
     {
         public int Id {  get; set; }
+        [Required]
+        [StringLength(30)]
         public string System_name { get; set; }
     }
 
     public class Mst_Status
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Status_Name { get; set; }
     }
 
     public class Mst_Communication
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(10)]
         public string Com_Name { get; set; }
     }
 
     public class Mst_Type
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Type_Name { get; set; }
     }
 
     public class Mst_User
     {
         public int Id { get; set; }
+        [StringLength(10)]
         public string Login_Id {get; set;}
         public string User_Name { get; set; }
         public string Password { get; set; }
@@ -38,6 +49,8 @@ namespace Deborah.Models
     public class Tra_Entry
     {
         public int Id { get; set; }
+        [Required]
+        [Phone]
         public string Tel_No { get; set; }
         public string Hostname { get; set; }
         public DateTime Entry_Time { get; set; }
@@ -60,7 +73,6 @@ namespace Deborah.Models
         public long Answer { get; set; }
         public long Cause { get; set; }
         public bool Complate_Flag { get; set; }
-        public bool Hachu_Flag { get; set;}
         public DateTime Entry_Time { get; set; }
         public DateTime End_Time { get; set; }
     }
