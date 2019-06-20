f8b9104c3284345e27dae6d8fe52ebe5d51b4dfc
kazusapon <flum1278pool@gmail.com>
2019-06-20 09:02:14 +0900

    testでござる

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DeborahCall.Models;
-
+//test
 namespace DeborahCall.Controllers
 {
     public class HomeController : Controller
