ff6302eded2c536ab53764eb0f15d30816a147e8
kazusapon <flum1278pool@gmail.com>
2019-07-09 17:23:18 +0900

    Userを追加

---
+++ Controllers/UserController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Deborah.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Inqury.Models;
+using Login.Filters;
+using Deborah_User;
+
+namespace UserController
+{
+    public class UserController : Controller
+    {
+        private readonly MyContext _context;
+        public UserController(MyContext context)
+        {
+            this._context = context;
+        }
+
+        [AuthorizationFilter]
+        [Route("User/Index")]
+        public IActionResult Index()
+        {
+            var _result = from usr in this._context.Mst_User
+                                orderby usr.Id
+                                select new User
+                                {
+                                    Id = usr.Id,
+                                    User_Name = usr.User_Name,
+                                    InquryCount = this._context.Tra_Inqury
+                                                      .Where(x => x.Login_Id == usr.Id)
+                                                      .Select(x => x.Id)
+                                                      .Count()
+                                };
+            return View(_result);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Deborah.Models;
+
+
+namespace Deborah_User
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string User_Name { get; set; }
+        public int InquryCount { get; set; }
+    }
+}
