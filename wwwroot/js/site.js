be6a72c43f89da1b1a99c0ee6d00731c96d2ef5a
UpperAndDown <flum1278pool@gmail.com>
2019-12-05 17:40:41 +0900

    111

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,3 +7,14 @@ function relation(ID) {
     var elm = document.getElementById("Relation_Id");
     elm.value = ID;
 }
+
+function Unknown() {
+    var elm = document.getElementById("Tel_No");
+    elm.value = "9999-99-9999";
+}
+
+//function CopySammary() {
+//    var textarea = document.getElementById("smy");
+//    textarea.select();
+//    document.execCommand("copy");
+//}
